de64b83dd9610efb3f0dbc03e98d2095d9f123b0
peter <p.oroshazi@gmail.com>
2019-06-16 16:03:25 +0200

    (nevnapp): name days file with dummy data

---
+++ nevnapp/lib/data/name_days.data.dart
@@ -0,0 +1,11 @@
+class NameDays {
+  final Map<DateTime, List> nameDays = {
+    DateTime(2019, 1, 1): ['New Year\'s Day'],
+    DateTime(2019, 1, 6): ['Epiphany'],
+    DateTime(2019, 2, 14): ['Valentine\'s Day'],
+    DateTime(2019, 4, 21): ['Easter Sunday'],
+    DateTime(2019, 4, 22): ['Easter Monday'],
+  };
+
+  Map<DateTime, List> returnNamedays() => nameDays;
+}
